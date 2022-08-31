1dad12ae5511477824283e69e72c622c71d37808
Caesar <98186985+caelanjmiller@users.noreply.github.com>
2022-08-31 17:11:56 -0400

    Reworked logic, added scoring, & DOM manipulations

--- RPS.js
+++ RPS.js
@@ -1,90 +1,245 @@
 // Simple Rock Paper Scissors Game 
 
-const choices = ['rock','paper', 'scissors'];
-//let rounds = parseInt(prompt('How many rounds of Rock, Paper, Scissors do you want to play?'));
+let playerName = document.querySelector('#player-name');
+let playerNameButton = document.querySelector("#psubmit");
+playerNameButton.addEventListener('click', userInput);
+function userInput () {
+    let input = document.getElementById('input').value;
+    playerName.innerHTML = input;
+}
 
 function getCpuChoice () {
-    let cpuChoice = choices[Math.floor(Math.random() * 3)]; // Random index from 0-2 [choices] to select CPU's move
+    const choices = ['rock','paper', 'scissors'];
+    cpuChoice = choices[Math.floor(Math.random() * 3)]; // Random index from 0-2 [choices] to select CPU's move
+    console.log(`CPU Choice: ${cpuChoice}`);
     return cpuChoice;
 }
 
-let cpuChoice = getCpuChoice();
-
-function playRound (cpuChoice) {
+const rockBtn = document.getElementById('rock');
+rockBtn.addEventListener('click', rockChoice);
+function rockChoice () {
+    console.log('The function rockChoice() is running....');
+    cpuButtonHighlightRemoval(cpuRock);
+    cpuButtonHighlightRemoval(cpuPaper);
+    cpuButtonHighlightRemoval(cpuScissors);
     let winnerOfRound = '';
-    let playerChoice = prompt('Choose From the Following Moves:\nRock\tPaper\tScissors').toLowerCase(); // Allows for case-insensitive input from user
-
+    let cpuChoice = getCpuChoice();
+    let playerChoice = 'rock';
     if (playerChoice === cpuChoice) {
         winnerOfRound = 'Draw, there is no winner';
-        return winnerOfRound;
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuRock);
+        playerCPUDraw(playerScore, cpuScore); 
+        return changeBannerText(winnerOfRound);
     }
     else if (playerChoice == 'rock' && cpuChoice === 'paper') {
         winnerOfRound = 'You lose! Paper beats rock!';
-        return winnerOfRound;
-    }
-    else if (playerChoice == 'paper' && cpuChoice === 'rock'){
-        winnerOfRound = 'You win! Paper beats rock!'
-        return winnerOfRound;
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuPaper);
+        cpuIncrementScore(cpuScore, cpuScoreElement);
+        return changeBannerText(winnerOfRound);
     }
     else if (playerChoice == 'rock' && cpuChoice === 'scissors') {
         winnerOfRound = 'You win! Rock beats scissors!';
-        return winnerOfRound;
-    }
-    else if (playerChoice == 'scissors' && cpuChoice === 'rock') {
-        winnerOfRound = 'You lose! Rock beats scissors!';
-        return winnerOfRound;
-    }
-    else if (playerChoice == 'paper' && cpuChoice === 'scissors') {
-        winnerOfRound = 'You lose! Scissors beats paper!';
-        return winnerOfRound;
-    }
-    else if (playerChoice == 'scissors' && cpuChoice === 'paper') {
-        winnerOfRound = 'You win! Scissors beats paper!';
-        return winnerOfRound;
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuScissors);
+        playerIncrementScore(playerScore, playerScoreElement);
+        return changeBannerText(winnerOfRound);
     }
     return winnerOfRound;
 }
 
-function rpsGame (rounds) {
-    let numRounds = rounds - 1;
-    let playerScore = 0;
-    let cpuScore = 0;
-    for (i = 0; i <= numRounds; i++) {
-        let roundEnd = playRound(cpuChoice);
-        console.log(roundEnd);
-        let words = roundEnd.split(' '); // Lines 53-63 allow for tracking of returned value from playRound(); if returned string contains 'win!', this is considered a +1 for player due to framing of statement, vice versa for 'lose!' +1 for CPU
-        if (words[1] == 'win!') {
-            playerScore += 1;
-        }
-        else if (words[1] == 'lose!') {
-            cpuScore += 1;
-        }
-        else {
-            cpuScore += 0;
-            playerScore += 0;
-        }
+const paperBtn = document.getElementById('paper');
+paperBtn.addEventListener('click', playRoundPaperChoice);
+function playRoundPaperChoice () {
+    console.log('The function paperChoice() is running....');
+    cpuButtonHighlightRemoval(cpuRock);
+    cpuButtonHighlightRemoval(cpuPaper);
+    cpuButtonHighlightRemoval(cpuScissors);
+    let winnerOfRound = '';
+    let cpuChoice = getCpuChoice();
+    let playerChoice = 'paper';
+    if (playerChoice === cpuChoice) {
+        winnerOfRound = 'Draw, there is no winner';
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuPaper);
+        playerCPUDraw(playerScore, cpuScore);
+        return changeBannerText(winnerOfRound);
     }
-    if (playerScore > cpuScore) {
-        grandWinner = `You are the Grand Winner! You won ${playerScore} out of ${rounds} rounds`;
-        console.log(grandWinner);
-        return grandWinner;
+    else if (playerChoice == 'paper' && cpuChoice === 'rock'){
+        winnerOfRound = 'You win! Paper beats rock!';
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuRock);
+        playerIncrementScore(playerScore, playerScoreElement);
+        return changeBannerText(winnerOfRound);
     }
-    else {
-        grandWinner = `Sadly you are not the Grand Winner. You only won ${playerScore} out of ${rounds} rounds`;
-        console.log(grandWinner);
-        return grandWinner;
+    else if (playerChoice == 'paper' && cpuChoice === 'scissors') {
+        winnerOfRound = 'You lose! Scissors beats paper!';
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuScissors);
+        cpuIncrementScore(cpuScore, cpuScoreElement);
+        return changeBannerText(winnerOfRound);
     }
+    return winnerOfRound;
 }
 
-function playerButtonSelection () {
-  playerButtonChoice = this.id.toLowerCase(); // this refers to DOM object (in this case button), allowing me call .id method and get id of said button
-  console.log(playerButtonChoice);
+const scissorsBtn = document.getElementById('scissors');
+scissorsBtn.addEventListener('click', playRoundScissorsChoice);
+function playRoundScissorsChoice() {
+    console.log('The function paperChoice() is running....');
+    cpuButtonHighlightRemoval(cpuRock);
+    cpuButtonHighlightRemoval(cpuPaper);
+    cpuButtonHighlightRemoval(cpuScissors);
+    let winnerOfRound = '';
+    let cpuChoice = getCpuChoice();
+    let playerChoice = 'scissors';
+    if (playerChoice === cpuChoice) {
+        winnerOfRound = 'Draw, there is no winner';
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuScissors);
+        playerCPUDraw(playerScore, cpuScore);
+        return changeBannerText(winnerOfRound);
+    }
+    else if (playerChoice == 'scissors' && cpuChoice === 'rock') {
+        winnerOfRound = 'You lose! Rock beats scissors!';
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuRock);
+        cpuIncrementScore(cpuScore, cpuScoreElement);
+        return changeBannerText(winnerOfRound);
+    }
+    else if (playerChoice == 'scissors' && cpuChoice === 'paper') {
+        winnerOfRound = 'You win! Scissors beats paper!';
+        console.log(winnerOfRound);
+        cpuButtonHighlight(cpuPaper);
+        playerIncrementScore(playerScore, playerScoreElement);
+        return changeBannerText(winnerOfRound);
+    }
+    return winnerOfRound;
 }
 
-const btns = document.querySelectorAll(".btn");
-btns.forEach(btn => btn.addEventListener('click',playerButtonSelection));
+const cpuScissors = document.querySelector('#cpu-scissors');
+const cpuPaper = document.querySelector('#cpu-paper');
+const cpuRock = document.querySelector('#cpu-rock');
+function cpuButtonHighlight(cpuBtn) { 
+    console.log('The function cpuButtonHighlight() is now running....');
+    let cpuSelection = cpuBtn;
+    cpuSelection.classList.add('cpu-selection');
+}
+function cpuButtonHighlightRemoval(cpuBtn){
+    console.log('The function cpuButtonHighLightRemoval() is now running');
+    let cpuSelection = cpuBtn;
+    cpuSelection.classList.remove('cpu-selection');
+}
 
+const announcementBanner = document.querySelector('.announcement');
+function changeBannerText(winnerOfRound) {
+    console.log('The function changeBannerText() is now running....');
+    let bannerText = winnerOfRound;
+    announcementBanner.innerHTML = bannerText;
+}
 
+var playerScore = 0;
+var cpuScore = 0;
+const playerScoreElement = document.querySelector('#player-score');
+const cpuScoreElement = document.querySelector('#cpu-score');
+function playerIncrementScore (score, scoreElement) {    
+    console.log('The function playerIncrementScore() is now running....');
+    playerScore++;
+    playerScoreElement.innerHTML = `Score: ${playerScore}`;
+}
+function cpuIncrementScore (score, scoreElement) {
+    console.log('The function cpuIncrementScore() is now running....');
+    cpuScore++;
+    cpuScoreElement.innerHTML = `Score: ${cpuScore}`;
+}
+function playerCPUDraw(playerScore, cpuScore) {
+    cpuScore += 0;
+    playerScore += 0;
+}
+// Legacy Code base
+// const pbtns = document.querySelectorAll('.pbtn');
+// pbtns.forEach(btns => btns.addEventListener('click', playerButtonSelection));
+// pbtns.forEach(btns => btns.addEventListener('click', playRound));
 
+// function playerButtonSelection () {
+//     playerButtonChoice = this.id.toLowerCase().toString(); // this refers to DOM object (in this case button), allowing me call .id method and get id of said button
+//     console.log(this);
+//     console.log(typeof(playerButtonChoice))
+//     console.log(`Player Choice: ${playerButtonChoice}`);
+//     return playerButtonChoice;
+// }
+// function playRound (playerButtonChoice) {
+//     console.log('The function playRound() is running....');
+//     let winnerOfRound = '';
+//     let cpuChoice = getCpuChoice();
+//     let playerChoice = playerButtonChoice;
+//     console.log(`playerButtonChoice variable value: ${playerButtonChoice}`);
+//     if (playerChoice === cpuChoice) {
+//         winnerOfRound = 'Draw, there is no winner';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     else if (playerChoice == 'rock' && cpuChoice === 'paper') {
+//         winnerOfRound = 'You lose! Paper beats rock!';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     else if (playerChoice == 'paper' && cpuChoice === 'rock'){
+//         winnerOfRound = 'You win! Paper beats rock!';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     else if (playerChoice == 'rock' && cpuChoice === 'scissors') {
+//         winnerOfRound = 'You win! Rock beats scissors!';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     else if (playerChoice == 'scissors' && cpuChoice === 'rock') {
+//         winnerOfRound = 'You lose! Rock beats scissors!';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     else if (playerChoice == 'paper' && cpuChoice === 'scissors') {
+//         winnerOfRound = 'You lose! Scissors beats paper!';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     else if (playerChoice == 'scissors' && cpuChoice === 'paper') {
+//         winnerOfRound = 'You win! Scissors beats paper!';
+//         console.log(winnerOfRound);
+//         return winnerOfRound;
+//     }
+//     return winnerOfRound;
+// }
+// function rpsGame (rounds) {
+//     let numRounds = rounds - 1;
+//     let playerScore = 0;
+//     let cpuScore = 0;
+//     for (i = 0; i <= numRounds; i++) {
+//         let roundEnd = playRound(cpuChoice);
+//         console.log(roundEnd);
+//         let words = roundEnd.split(' '); // Lines 53-63 allow for tracking of returned value from playRound(); if returned string contains 'win!', this is considered a +1 for player due to framing of statement, vice versa for 'lose!' +1 for CPU
+//         if (words[1] == 'win!') {
+//             playerScore += 1;
+//         }
+//         else if (words[1] == 'lose!') {
+//             cpuScore += 1;
+//         }
+//         else {
+//             cpuScore += 0;
+//             playerScore += 0;
+//         }
+//     }
+//     if (playerScore > cpuScore) {
+//         grandWinner = `You are the Grand Winner! You won ${playerScore} out of ${rounds} rounds`;
+//         console.log(grandWinner);
+//         return grandWinner;
+//     }
+//     else {
+//         grandWinner = `Sadly you are not the Grand Winner. You only won ${playerScore} out of ${rounds} rounds`;
+//         console.log(grandWinner);
+//         return grandWinner;
+//     }
+// }
 
 // Caelan Miller - 2022
